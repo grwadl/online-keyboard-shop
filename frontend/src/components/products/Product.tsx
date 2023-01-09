20f2fe955ecee13a90480ff6886577880215cfaa
grwadl <youngwwad@gmail.com>
2023-01-09 18:31:04 +0200

    added lazy loading component

--- frontend/src/components/products/Product.tsx
+++ frontend/src/components/products/Product.tsx
@@ -1,19 +1,25 @@
 import cart from '@/assets/icons/white-cart.svg'
+import placeholder from '@/assets/shop-placeholder.png'
 import { IProduct } from '@/redux/types/reducers/products'
 import { Link } from 'react-router-dom'
 import Button from '../UI/Button'
+import { LazyLoad } from '../UI/lazy-load/LazyLoad'
 import './product.scss'
 
 type Props = {
   product: IProduct
   className?: string
 }
-const placeholder = 'https://returntofreedom.org/store/wp-content/uploads/shop-placeholder.png'
 
 const Product = ({ product, className }: Props) => {
   return (
     <Link to={'/product/' + product.id} className={`product-item bg-white sm:px-2 py-2 ${className}`}>
-      <img src={product?.image ?? placeholder} alt="product image" className="block object-cover h-48 w-full" />
+      <LazyLoad
+        alt="product image"
+        placeholder={placeholder}
+        src={product?.image ?? placeholder}
+        className="block object-cover h-48 w-full"
+      />
       <h3 className="product-title block ">{product.name}</h3>
 
       <Button className="flex mt-2 gap-1 items-center text-sm">
