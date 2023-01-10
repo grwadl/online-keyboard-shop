819d2bce5134996519f100cfd1e5d7dcf8464ad9
grwadl <youngwwad@gmail.com>
2023-01-10 13:00:45 +0200

    fixed typo

--- frontend/src/components/product-page/info-panels/mock.ts
+++ frontend/src/components/product-page/info-panels/mock.ts
@@ -3,17 +3,17 @@ import { Text } from '../HeaderAndTextBlock'
 const shippingKyivText: Text[] = [
   { leftSide: 'from 6000$', rightSide: 'free' },
   { leftSide: 'from 3000$ to 6000$', rightSide: '300$' },
-  { leftSide: 'below 3000$', rightSide: '500$' }
+  { leftSide: 'less than 3000$', rightSide: '500$' }
 ]
 
 const shippingKyivRegionText: Text[] = [
   { leftSide: 'from 3000$ to 6000$', rightSide: '500$' },
-  { leftSide: 'below 3000$', rightSide: '6s00$' }
+  { leftSide: 'less than 3000$', rightSide: '6000$' }
 ]
 
 const shippingOtherCitiesText: Text[] = [
   { leftSide: 'more than 6000$', rightSide: 'free' },
-  { leftSide: 'less than 6000%', rightSide: '1000$' }
+  { leftSide: 'less than 6000$', rightSide: '1000$' }
 ]
 
 const paymentKyivAndRegionText: Text[] = [{ leftSide: 'Prepayment or after delivery' }]
